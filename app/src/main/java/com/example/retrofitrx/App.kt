86779e6371bffcb4935a20ac8b468dae5aec2628
borhammere <borhammere@gmail.com>
2022-04-14 21:39:13 +0300

    Implement retrofit repo

--- app/src/main/java/com/example/retrofitrx/App.kt
+++ app/src/main/java/com/example/retrofitrx/App.kt
@@ -2,11 +2,11 @@ package com.example.retrofitrx
 
 import android.app.Application
 import android.content.Context
-import com.example.retrofitrx.data.MockProjectsRepoImpl
+import com.example.retrofitrx.data.retrofit.RetrofitProjectsRepoImpl
 import com.example.retrofitrx.domain.ProjectsRepo
 
 class App : Application() {
-    val gitProjectsRepo: ProjectsRepo by lazy { MockProjectsRepoImpl() }
+    val gitProjectsRepo: ProjectsRepo by lazy { RetrofitProjectsRepoImpl() }
 }
 
 val Context.app: App
--- app/src/main/java/com/example/retrofitrx/ui/ReposViewModel.kt
+++ app/src/main/java/com/example/retrofitrx/ui/ReposViewModel.kt
@@ -11,13 +11,19 @@ import io.reactivex.rxjava3.kotlin.subscribeBy
 class ReposViewModel(private val gitProjectRepo: ProjectsRepo) : ViewModel() {
     private val _repos = MutableLiveData<List<GitProjectEntity>>()
     val repos: LiveData<List<GitProjectEntity>> = _repos
+
+    private val _inProgress = MutableLiveData<Boolean>()
+    val inProgress: LiveData<Boolean> = _inProgress
+
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     fun onShowRepos(username: String) {
+        _inProgress.postValue(true)
         compositeDisposable.add(
             gitProjectRepo
                 .observeReposForUser(username)
                 .subscribeBy {
+                    _inProgress.postValue(false)
                     _repos.postValue(it)
                 }
         )
